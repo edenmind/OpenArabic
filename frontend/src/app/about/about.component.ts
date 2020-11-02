7925b8314c93e0f981dde2f2db239f5d17e48171
Yûnus Andréasson <yunus@edenmind.com>
2020-11-02 20:53:19 +0100

    refactoring of html

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
