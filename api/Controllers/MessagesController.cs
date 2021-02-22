aea8a704a5382a6768f02bb88702b7b9fe5cd933
Yûnus Andréasson <yunus@edenmind.com>
2021-02-22 23:43:16 +0100

    semifix for messages not arriving

--- api/Controllers/MessagesController.cs
+++ api/Controllers/MessagesController.cs
@@ -1,38 +1,36 @@
 using System.Threading.Tasks;
+
 using api.Models;
 using api.Services;
 using api.Validators;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace api.Controllers
-{
+namespace api.Controllers {
     [ApiController]
-    public class MessagesController : ControllerBase
-    {
+    public class MessagesController : ControllerBase {
         private readonly IMessageService _messageService;
 
-        public MessagesController(IMessageService messageService)
-        {
+        public MessagesController (IMessageService messageService) {
             _messageService = messageService;
         }
 
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [AllowAnonymous]
         [HttpPost]
-        [Route("api/messages")]
-        public async Task<ActionResult<string>> PostMessage(Mail mail)
-        {
-            MailValidator validator = new();
+        [Route ("api/messages")]
+        public async Task<ActionResult<string>> PostMessage (Mail mail) {
+            MailValidator validator = new ();
 
-            var validationResult = await validator.ValidateAsync(mail);
+            var validationResult = await validator.ValidateAsync (mail);
 
-            if (!validationResult.IsValid) return BadRequest(validationResult);
+            if (!validationResult.IsValid) return BadRequest (validationResult);
 
-            await _messageService.SendMessage(mail.Sender, mail.Body);
+            await _messageService.SendMessage (mail.Sender, mail.Body);
 
             //TODO: Error handling
-            return Ok();
+            return Ok ();
         }
     }
 }
--- api/Services/MessageService.cs
+++ api/Services/MessageService.cs
@@ -1,21 +1,20 @@
 using System;
 using System.Threading.Tasks;
+
 using SendGrid;
 using SendGrid.Helpers.Mail;
 
-namespace api.Services
-{
-    public class MessageService : IMessageService
-    {
-        public async Task SendMessage(string sender, string body)
-        {
-            var apiKey = Environment.GetEnvironmentVariable("ASPNETCORE_SENDGRID_KEY");
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress(sender, "OpenArabic Form Message");
-            const string subject = "Message from OpenArabic";
-            var to = new EmailAddress("salam@edenmind.com", "Support");
-            var message = MailHelper.CreateSingleEmail(from, to, subject, body, "");
-            var response = await client.SendEmailAsync(message);
+namespace api.Services {
+    public class MessageService : IMessageService {
+        public async Task SendMessage (string sender, string body) {
+            var apiKey = Environment.GetEnvironmentVariable ("ASPNETCORE_SENDGRID_KEY");
+            var client = new SendGridClient (apiKey);
+            var from = new EmailAddress ("yunus@edenmind.com", "OpenArabic Form Message");
+            string subject = "Message from: " + sender;
+            var to = new EmailAddress ("yunus@edenmind.com", "Support");
+            var message = MailHelper.CreateSingleEmail (from, to, subject, body, "");
+            var response = await client.SendEmailAsync (message);
+            var x = "";
         }
     }
 }
