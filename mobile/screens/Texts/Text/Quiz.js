ec399d898a4ca33eaa4c7fed60616cf5a19930f4
Yunus Andréasson <yunus@edenmind.com>
2022-03-15 20:41:17 +0100

    Quiz for vocabulary Fixes #621 wip
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- mobile/screens/Texts/Text/Quiz.js
+++ mobile/screens/Texts/Text/Quiz.js
@@ -6,36 +6,6 @@ import Spinner from '../../../components/Spinner'
 import { StyleSheet, View } from 'react-native'
 
 const Quiz = () => {
-  const selector = (state) => state.text
-  const { text } = useSelector(selector)
-
-  const arabicSelected = Array(5)
-    .fill()
-    .map(() => true)
-
-  const englishSelected = Array(5)
-    .fill()
-    .map(() => true)
-
-  const [arabicSelectedState, setSelectedArabic] =
-    React.useState(arabicSelected)
-  const [englishSelectedState, setSelectedEnglish] =
-    React.useState(englishSelected)
-
-  const setFuncArabic = (index) => {
-    console.log('index: ', index)
-    const currentValue = arabicSelectedState[index]
-    arabicSelectedState[index] = !currentValue
-    setSelectedArabic([...arabicSelectedState])
-  }
-
-  const setFuncEnglish = (index) => {
-    console.log('index: ', index)
-    const currentValue = englishSelectedState[index]
-    englishSelectedState[index] = !currentValue
-    setSelectedEnglish([...englishSelectedState])
-  }
-
   const styles = StyleSheet.create({
     chip: {
       flexDirection: 'row',
@@ -44,37 +14,90 @@ const Quiz = () => {
     }
   })
 
-  if (text.title) {
-    const arabicVocabularies = text.vocabularyCollection.arabic.map(
-      (arabic, index) => (
-        <ChipText
-          key={arabic.wordId}
-          text={arabic.word}
-          func={() => setFuncArabic(index)}
-          selected={arabicSelectedState[index]}
-        />
-      )
-    )
+  const selector = (state) => state.text
+  const { text } = useSelector(selector)
 
-    const englishVocabularies = text.vocabularyCollection.english.map(
-      (english, index) => (
-        <ChipText
-          key={english.wordId}
-          text={english.word}
-          func={() => setFuncEnglish(index)}
-          selected={englishSelectedState[index]}
-        />
-      )
-    )
+  //current selected
+  const [arabicCurrentSelected, setArabicCurrentSelected] = React.useState()
+  const [englishCurrentSelected, setEnglishCurrentSelected] = React.useState()
 
-    return (
-      <>
-        <View style={styles.chip}>{arabicVocabularies}</View>
-        <View style={styles.chip}>{englishVocabularies}</View>
-      </>
-    )
+  //selected, unselected, disabled
+  const arabicSelected = Array(
+    text.vocabularyCollection.arabic
+      ? text.vocabularyCollection.arabic.length
+      : 0
+  )
+    .fill()
+    .map(() => false)
+
+  const englishSelected = Array(
+    text.vocabularyCollection.english
+      ? text.vocabularyCollection.english.length
+      : 0
+  )
+    .fill()
+    .map(() => false)
+
+  const [arabicSelectedState, setSelectedArabic] =
+    React.useState(arabicSelected)
+
+  const [englishSelectedState, setSelectedEnglish] =
+    React.useState(englishSelected)
+
+  const setFuncArabic = (index, wordId) => {
+    const currentValue = arabicSelectedState[index]
+    arabicSelectedState[index] = !currentValue
+    setSelectedArabic([...arabicSelectedState])
+    setArabicCurrentSelected(wordId)
+    if (wordId == englishCurrentSelected) {
+      console.log('match')
+    } else {
+      console.log('no match')
+    }
   }
-  return <Spinner />
+
+  const setFuncEnglish = (index, wordId) => {
+    const currentValue = englishSelectedState[index]
+    englishSelectedState[index] = !currentValue
+    setSelectedEnglish([...englishSelectedState])
+    setEnglishCurrentSelected(wordId)
+    if (arabicCurrentSelected == wordId) {
+      console.log('match')
+    } else {
+      console.log('no match')
+    }
+  }
+
+  const arabicVocabularies = text.vocabularyCollection.arabic.map(
+    (arabic, index) => (
+      <ChipText
+        key={arabic.wordId}
+        text={arabic.word}
+        func={() => setFuncArabic(index, arabic.wordId)}
+        selected={arabicSelectedState[index]}
+      />
+    )
+  )
+
+  const englishVocabularies = text.vocabularyCollection.english.map(
+    (english, index) => (
+      <ChipText
+        key={english.wordId}
+        text={english.word}
+        func={() => setFuncEnglish(index, english.wordId)}
+        selected={englishSelectedState[index]}
+      />
+    )
+  )
+
+  return text.title ? (
+    <>
+      <View style={styles.chip}>{arabicVocabularies}</View>
+      <View style={styles.chip}>{englishVocabularies}</View>
+    </>
+  ) : (
+    <Spinner />
+  )
 }
 
 export default Quiz
