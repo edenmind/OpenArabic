ff6bbc9b36d8f29b8bda792075442d9c60083514
Yunus Andréasson <yunus@edenmind.com>
2022-04-06 20:34:45 +0200

    Basic admin interface with auth Fixes #675
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- frontend/src/components/MenuSelect.js
+++ frontend/src/components/MenuSelect.js
@@ -4,12 +4,13 @@ const MenuSelect = (props) => (
   <FormControl fullWidth>
     <InputLabel id={props.Heading}>{props.Heading}</InputLabel>
     <Select labelId={props.Heading} id={props.Heading} value={props.value} label={props.Heading} onChange={props.onChangeFunc}>
-      {props.Values &&
-        props.Values.map((v, index) => (
-          <MenuItem key={index} value={v.categroyId ? v.categroyId : v.authorId}>
-            {v.name}
-          </MenuItem>
-        ))}
+      {props.Values?.map((v, index) => (
+        <MenuItem key={index} value={v.categroyId ? v.categroyId : v.authorId}>
+          {' '}
+          {/* this should be refactored. */}
+          {v.name}
+        </MenuItem>
+      ))}
     </Select>
   </FormControl>
 )
--- frontend/src/screens/Words.js
+++ frontend/src/screens/Words.js
@@ -1,4 +1,4 @@
-import { Divider, MenuItem, Select } from '@mui/material'
+import { Container, Divider, FormControl, MenuItem, Select } from '@mui/material'
 
 import React from 'react'
 import { useSelector } from 'react-redux'
@@ -6,24 +6,32 @@ import { useSelector } from 'react-redux'
 const Words = () => {
   const { englishWords } = useSelector((state) => state.englishWords)
   const { arabicWords } = useSelector((state) => state.arabicWords)
+  const [theArabicWord, setTheArabicWord] = React.useState([])
 
+  const handleChangeArabic = (event) => {
+    console.log(event.target.value)
+  }
   const listOfWords = englishWords.map((sentence, indexSentence) => (
     <div key={indexSentence}>
-      Sentence: {indexSentence}
+      <strong>Sentence: {indexSentence}</strong>
+      <br />
+      <br />
       {sentence.map((word, indexWord) => (
-        <div key={indexWord}>
-          <div>Word: {word}</div>
-
-          <Select value='10' label='Age'>
-            {arabicWords[indexSentence].map((arabicWord, indexArabicWord) => (
-              <MenuItem key={indexArabicWord} value={arabicWord}>
-                {arabicWord}
-              </MenuItem>
-            ))}
-          </Select>
-        </div>
+        <Container key={indexWord}>
+          <div>{word}</div>
+          <FormControl fullWidth>
+            <Select value={theArabicWord[indexWord]} onChange={handleChangeArabic}>
+              {arabicWords[indexSentence].map((arabicWord, indexArabicWord) => (
+                <MenuItem key={indexArabicWord} value={arabicWord}>
+                  {arabicWord}
+                </MenuItem>
+              ))}
+            </Select>
+            <br />
+          </FormControl>
+        </Container>
       ))}
-      <Divider />
+      <br />
     </div>
   ))
 
