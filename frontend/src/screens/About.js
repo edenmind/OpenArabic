f14c5657a3e83594beba0c1ea7fbc9caaa32845f
Yunus Andréasson <yunus@edenmind.com>
2022-04-11 12:38:39 +0200

    Basic admin interface with auth Fixes #675
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -1,18 +1,54 @@
-import { Container } from '@mui/material'
+import { Box, Container, Link, List, ListItem, ListItemText, Typography } from '@mui/material'
+
 import Nav from './Nav'
 import React from 'react'
+import axios from 'axios'
 
 const About = () => {
+  const [authors, setAuthors] = React.useState([])
+  React.useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/authors`)
+      .then((response) => {
+        setAuthors(response.data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  const authorsList = authors.map((author, index) => (
+    <ListItem key={index}>
+      <Typography variant='span'>{author.name}</Typography>
+    </ListItem>
+  ))
+
   return (
     <React.Fragment>
       <Nav />
       <Container maxWidth='lg'>
-        <div>
-          <h2>Audience</h2>
-          If you know some Arabic and strive to switch from reading Islamic texts in English to read in Arabic; then OpenArabic is a reading platform — featuring short bilingual texts and vocabulary
-          quizzes — that will help you in that process, inshāʾAllāh. OpenArabic does not teach the Arabic alphabet nor Arabic grammar. If you need a resource to get you started, then the Duolingo app
-          available on iPhone and Android might come in handy.
-        </div>
+        <Typography component='div'>
+          <Box sx={{ fontSize: 'h6.fontSize', m: 2 }}>بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ</Box>
+
+          <Box sx={{ fontSize: 'h5.fontSize', m: 2 }}>Audience</Box>
+
+          <Box sx={{ m: 2 }}>
+            If you know some Arabic and strive to switch from reading Islamic texts in English to read in Arabic; then OpenArabic is a reading platform — featuring short bilingual texts and vocabulary
+            quizzes — that will help you in that process, inshāʾAllāh
+          </Box>
+
+          <Box sx={{ m: 2 }}>
+            OpenArabic does not teach the Arabic alphabet nor Arabic grammar. If you need a resource to get you started, then the Duolingo app available on{' '}
+            <Link href='https://apps.apple.com/us/app/duolingo-language-lessons/id570060128'>iPhone</Link> and
+            <Link href='https://play.google.com/store/apps/details?id=com.duolingo&hl=en_US&gl=US'> Android </Link>
+            might come in handy.
+          </Box>
+          <Box sx={{ fontSize: 'h5.fontSize', m: 2 }}>Foundation</Box>
+
+          <Box sx={{ m: 2 }}>
+            The theological foundation of OpenArabic is based upon the Qurʼān, the Prophetic Sunnah and the first generations of Muslims understanding with texts from Islamic Scholars such as:
+          </Box>
+
+          <List>{authorsList}</List>
+        </Typography>
       </Container>
     </React.Fragment>
   )
--- frontend/src/screens/Contact.js
+++ frontend/src/screens/Contact.js
@@ -1,4 +1,5 @@
-import { Container } from '@mui/material'
+import { Box, Button, Container, Stack } from '@mui/material'
+
 import Nav from './Nav'
 import React from 'react'
 
@@ -7,10 +8,32 @@ const Contact = () => {
     <React.Fragment>
       <Nav />
       <Container maxWidth='lg'>
-        <div>
-          <h2>Audience</h2>
-          If you find any bugs 🐛 or have any feature suggestions 💭 on how to improve OpenArabic, then please use this contact form or raise 🚀 an issue on Github.
-        </div>
+        <Box sx={{ fontSize: 'h5.fontSize', m: 2 }}>Contact</Box>
+        <Box sx={{ m: 2 }}>If you find any bugs 🐛 or have any feature suggestions 💭 on how to improve OpenArabic, then please contact us.</Box>
+
+        <Box sx={{ m: 2 }}>
+          <Stack spacing={2} direction='row'>
+            <Button variant='contained' href='https://github.com/edenmind/OpenArabic'>
+              GitHub
+            </Button>
+
+            <Button variant='contained' href='mail:salam@edenmind.com'>
+              Mail
+            </Button>
+
+            <Button variant='contained' href='https://www.instagram.com/openarabic.io/'>
+              Instagram
+            </Button>
+
+            <Button variant='contained' href='https://twitter.com/OpenArabicIo'>
+              Twitter
+            </Button>
+
+            <Button variant='contained' href='https://www.facebook.com/OpenArabic.io/'>
+              Facebook
+            </Button>
+          </Stack>
+        </Box>
       </Container>
     </React.Fragment>
   )
