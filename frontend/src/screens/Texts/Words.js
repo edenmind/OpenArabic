80f1fecbbf7b57601e5cd8641807760b59ab583e
Yunus Andréasson <yunus@edenmind.com>
2022-04-14 01:07:20 +0200

    wip
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- frontend/src/screens/Texts/Words.js
+++ frontend/src/screens/Texts/Words.js
@@ -1,4 +1,4 @@
-import { Box, Checkbox, FormControlLabel, FormGroup, Stack, TextField } from '@mui/material'
+import { Box, Stack, TextField } from '@mui/material'
 import React, { useEffect } from 'react'
 
 import { useSelector } from 'react-redux'
@@ -6,48 +6,49 @@ import { useSelector } from 'react-redux'
 const Words = () => {
   const { englishWords } = useSelector((state) => state.englishWords)
   const { arabicWords } = useSelector((state) => state.arabicWords)
+  const { englishSentence } = useSelector((state) => state.englishSentence)
+  const { arabicSentence } = useSelector((state) => state.arabicSentence)
+
   const [theArabicWord, setTheArabicWord] = React.useState()
 
-  const handleChangeArabic = (englishWord, indexSentence, indexArabicWord) => {
+  const handleChangeArabic = (indexSentence, indexArabicWord, value) => {
     const newTheArabicWord = [...theArabicWord]
-    newTheArabicWord[indexSentence + 1][indexArabicWord].push(englishWord)
+    newTheArabicWord[indexSentence + 1][indexArabicWord] = value
     setTheArabicWord(newTheArabicWord)
-    console.log(englishWord, theArabicWord)
+    console.log(theArabicWord)
   }
 
   useEffect(() => {
     const newTheArabicWord = [[]]
 
-    const numberOfSentences = arabicWords.length
-
-    for (let i = 0; i < numberOfSentences; i++) {
-      const numberOfWords = arabicWords[i].length
+    for (const arabicWord of arabicWords) {
+      const numberOfWords = arabicWord.length
       const newArray = []
       for (let j = 0; j < numberOfWords; j++) {
-        newArray.push([])
+        newArray.push('')
       }
       newTheArabicWord.push(newArray)
     }
-    console.log(theArabicWord)
     setTheArabicWord(newTheArabicWord)
   }, [])
   const listOfWords = arabicWords.map((sentence, indexSentence) => (
     <Stack spacing={2} key={`${indexSentence}d`}>
-      <TextField InputProps={{ style: { fontSize: 25 } }} value={sentence} label='Arabic' multiline rows={1} fullWidth variant='filled' />
-      {sentence.map((arabicWord, indexArabicWord) => (
-        <>
-          <Box sx={{ fontSize: 'h4.fontSize' }} key={`${indexArabicWord}a`}>
-            {arabicWord}
-          </Box>
+      <TextField InputProps={{ style: { fontSize: 18 } }} value={englishSentence[indexSentence]} label='English' multiline rows={2} fullWidth variant='filled' />
+      <TextField InputProps={{ style: { fontSize: 25 } }} value={arabicSentence[indexSentence]} label='Arabic' multiline rows={1} fullWidth variant='filled' />
 
-          <Stack spacing={2} direction='row' key={`${indexArabicWord}b`}>
-            {englishWords[indexSentence].map((englishWord, indexEnglishWord) => (
-              <FormGroup key={`${indexEnglishWord}c`}>
-                <FormControlLabel control={<Checkbox onClick={() => handleChangeArabic(englishWord, indexSentence, indexArabicWord)} />} label={englishWord} />
-              </FormGroup>
-            ))}
-          </Stack>
-        </>
+      {sentence.map((arabicWord, indexArabicWord) => (
+        <Box sx={{ fontSize: 'h4.fontSize' }} key={`${indexArabicWord}a`}>
+          {arabicWord}
+          <TextField
+            InputProps={{ style: { fontSize: 17 } }}
+            value={theArabicWord[indexSentence][indexArabicWord]}
+            onChange={(event) => handleChangeArabic(indexSentence, indexArabicWord, event.target.value)}
+            multiline
+            rows={1}
+            fullWidth
+            variant='filled'
+          />
+        </Box>
       ))}
       <br />
       <br />
