943390e670b3a709eb44cc78fd9ed6cd4bb15a2b
Yunus Andréasson <yunus@edenmind.com>
2022-06-13 21:50:46 +0200

    add readiness probe for api fixes 872
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

---
+++ api/plugins/healthcheck.js
@@ -0,0 +1,8 @@
+'use strict'
+
+/* eslint-disable prefer-arrow-callback */
+const fastifyPlugin = require('fastify-plugin')
+
+module.exports = fastifyPlugin(async function (fastify) {
+  fastify.register(require('fastify-healthcheck'), {})
+})
