e311ded3b88b8ef21e5ff759a122ca724eca3b21
Yunus Andréasson <yunus@edenmind.com>
2022-06-18 20:33:24 +0200

    tweak init script
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- database/init-database-and-user.js
+++ database/init-database-and-user.js
@@ -1,3 +1,17 @@
+db = db.getSiblingDB('openarabic')
+db.createCollection('init_collection')
+db.init_collection.insertMany([
+  {
+    init: '1'
+  },
+  {
+    init: '2'
+  },
+  {
+    init: '3'
+  }
+])
+
 db.createUser({
   user: 'user',
   pwd: 'password',
