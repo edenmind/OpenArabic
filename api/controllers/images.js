de13539032e84bd21f7b23db6a678b77bdf825af
Yunus Andréasson <yunus@edenmind.com>
2022-06-22 21:51:34 +0200

    fix: images url
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- api/controllers/images.js
+++ api/controllers/images.js
@@ -4,31 +4,31 @@ function getImages(request, reply) {
   // eslint-disable-next-line putout/objects-braces-inside-array
   const headerImages = [
     {
-      img: `${process.env.API_IMAGE_URL}1.png`
+      img: `${process.env.API_IMAGES_URL}1.png`
     },
     {
-      img: `${process.env.API_IMAGE_URL}2.png`
+      img: `${process.env.API_IMAGES_URL}2.png`
     },
     {
-      img: `${process.env.API_IMAGE_URL}3.png`
+      img: `${process.env.API_IMAGES_URL}3.png`
     },
     {
-      img: `${process.env.API_IMAGE_URL}4.png`
+      img: `${process.env.API_IMAGES_URL}4.png`
     },
     {
-      img: `${process.env.API_IMAGE_URL}5.png`
+      img: `${process.env.API_IMAGES_URL}5.png`
     },
     {
-      img: `${process.env.API_IMAGE_URL}6.png`
+      img: `${process.env.API_IMAGES_URL}6.png`
     },
     {
-      img: `${process.env.API_IMAGE_URL}7.png`
+      img: `${process.env.API_IMAGES_URL}7.png`
     },
     {
-      img: `${process.env.API_IMAGE_URL}8.png`
+      img: `${process.env.API_IMAGES_URL}8.png`
     },
     {
-      img: `${process.env.API_IMAGE_URL}9.png`
+      img: `${process.env.API_IMAGES_URL}9.png`
     }
   ]
 
