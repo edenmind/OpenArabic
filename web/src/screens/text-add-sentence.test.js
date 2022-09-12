63698e4529909b772648f29c715c4fbdcfdb1a69
Yunus Andréasson <yunus@edenmind.com>
2022-09-12 22:58:26 +0200

    fix texts in web
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- web/src/screens/text-add-sentence.test.js
+++ web/src/screens/text-add-sentence.test.js
@@ -1,7 +1,7 @@
 import TextAddSentences from './text-add-sentence.js'
 import React from 'react'
 import ShallowRenderer from 'react-test-renderer/shallow' // ES6
-import { store } from '../redux/store.js'
+import store from '../redux/store.js'
 import { Provider } from 'react-redux'
 
 it('renders without crashing', () => {
