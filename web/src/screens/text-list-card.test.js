9ac12c72c8332bc1f8bb227b14010449305c15e7
Yunus Andréasson <yunus@edenmind.com>
2022-09-24 15:05:05 +0200

    Add english peek to card in ingress Fixes #1127
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- web/src/screens/text-list-card.test.js
+++ web/src/screens/text-list-card.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable putout/objects-braces-inside-array */
 import TextListCard from './text-list-card.js'
 import React from 'react'
 import ShallowRenderer from 'react-test-renderer/shallow' // ES6
@@ -5,6 +6,29 @@ import ShallowRenderer from 'react-test-renderer/shallow' // ES6
 it('renders without crashing', () => {
   const renderer = new ShallowRenderer()
 
-  const texts = [{ category: '', id: '', title: '', text: '', author: '', sentences: [] }]
+  const texts = [
+    {
+      arabic: [''],
+      english: [''],
+      status: 'Draft',
+      publishAt: new Date().toUTCString(),
+      wordByWord: [['']],
+      title: '',
+      texts: { arabic: '', english: '' },
+      category: '',
+      author: '',
+      arabicSentence: [''],
+      source: '',
+      // eslint-disable-next-line putout/objects-braces-inside-array
+      sentences: [
+        {
+          english: '',
+          arabic: '',
+          words: []
+        }
+      ]
+    }
+  ]
+
   renderer.render(<TextListCard texts={texts} />)
 })
