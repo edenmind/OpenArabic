f76639a73124ac758fdb4e5c857a13a897f6e5ce
Yunus Andréasson <yunus@edenmind.com>
2022-10-09 21:50:37 +0200

    add dummy busnag key
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- api/plugins/bugsnag.js
+++ api/plugins/bugsnag.js
@@ -5,6 +5,6 @@ const fastifyPlugin = require('fastify-plugin')
 
 module.exports = fastifyPlugin(async function (fastify) {
   fastify.register(require('fastify-bugsnag'), {
-    apiKey: fastify.config.API_BUGSNAG_KEY
+    apiKey: fastify.config.API_BUGSNAG_KEY || 'abc'
   })
 })
