eabf6101eb565851f9df3f6bcb742a0b0212b858
Yunus Andréasson <yunus@edenmind.com>
2022-10-23 00:13:21 +0200

    Add code codecoverage at buildtime Fixes #912
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   automock: false,
 
   // Stop running tests after `n` failures
-  // bail: 0,
+  bail: 1,
 
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "/private/var/folders/g_/g3vyp6ls5_3gf_8vqnl5tl940000gn/T/jest_dx",
