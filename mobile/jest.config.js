161dd732a1622a87d2ca57ddca60cbfba53b9d4c
Yunus Andréasson <yunus@edenmind.com>
2022-10-23 00:46:18 +0200

    switch to cobertura for test reports
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -26,20 +26,13 @@ module.exports = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ['./node_modules/'],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: ['cobertura'],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
