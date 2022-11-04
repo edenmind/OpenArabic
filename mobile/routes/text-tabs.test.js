5dc475f7c66eb96d36616671e180959337f87177
Yunus Andréasson <yunus@edenmind.com>
2022-11-04 21:39:00 +0100

    test(mobile): :white_check_mark: increase test coverage #1358
    Fixes #1358
    
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- mobile/routes/text-tabs.test.js
+++ mobile/routes/text-tabs.test.js
@@ -1,13 +1,28 @@
 import { describe, it, jest } from '@jest/globals'
 import ShallowRenderer from 'react-test-renderer/shallow' // ES6
 import React from 'react'
-import Settings from './settings.js'
+import TextTabs from './text-tabs.js'
+import { store } from '../redux/store.js'
+import { NavigationContainer } from '@react-navigation/native'
+import { Provider } from 'react-redux'
 
-jest.useFakeTimers()
-describe('<AboutNavigator />', () => {
-  it('renders without crashing', () => {
-    // @ts-ignore
+describe('<TextTabs />', () => {
+  it('when isLoading is true, should match snapshot', () => {
+    //mock id router.params
+    const route = {
+      params: {
+        id: '1'
+      }
+    }
     const renderer = new ShallowRenderer()
-    renderer.render(<Settings />)
+    const tree = renderer.render(
+      <Provider store={store}>
+        <NavigationContainer>
+          <TextTabs route={route} />
+        </NavigationContainer>
+      </Provider>
+    )
+
+    expect(tree).toMatchSnapshot()
   })
 })
