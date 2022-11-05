53d4db3a03a5288c21401c1648952361bbd56011
Yunus Andréasson <yunus@edenmind.com>
2022-11-05 11:25:56 +0100

    refactor(web): :fire: clean up some texts from startpage #1407
    Fixes #1407
    
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- web/src/screens/text-list-heading-home.js
+++ web/src/screens/text-list-heading-home.js
@@ -23,12 +23,6 @@ export const TextListHeadingHome = (properties) => {
               texts. The target audience is primarily non-native speakers of Arabic who are interested in learning the
               language for the purpose of reading the Quran and other Islamic texts.
             </p>
-            <p>
-              OpenArabic is based upon the Qurʼān, the Prophetic Sunnah and the first generations of Muslims
-              understanding with texts from Islamic Scholars such as: al-Hasan al-Basri, Imām Abū Ḥanīfa, Imām Mālik bin
-              Anas, Imām al-Shāfiʿī, Imām Aḥmad ibn Ḥanbal, Ibn Rājab al-Hanbali, Ibn Taymiyyah, Ibn Qayyim al-Jawziyya,
-              Shams ad-Dīn adh-Dhahabī, Imām Nawawī, Ibn Kathīr, Ibn Ḥajar al-ʿAsqalānī and al-Fuḍayl ibn ʻIyāḍ.
-            </p>
           </Typography>
           <Typography sx={{ mb: 1.5 }}>
             <h4>{properties.subHeading}</h4>
