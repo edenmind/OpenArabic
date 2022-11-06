d816a824b6960b4932e8834bf455e0d845eca5f9
Yunus Andréasson <yunus@edenmind.com>
2022-11-06 01:36:45 +0100

    fix(web): :white_check_mark: silence warnings in tests #1435
    Fixes #1435
    
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- web/src/screens/categories-list.test.js
+++ web/src/screens/categories-list.test.js
@@ -1,16 +1,18 @@
 import CategoriesList from './categories-list.js'
 import React from 'react'
 import ShallowRenderer from 'react-test-renderer/shallow' // ES6
-
-const handleClickOpen = () => {}
 it('renders without crashing', () => {
+  const handleClickOpenDummy = () => {
+    console.log('handleClickOpen')
+  }
+
   const renderer = new ShallowRenderer()
 
   // eslint-disable-next-line putout/objects-braces-inside-array
   const categories = [
-    { name: 'Category 1', id: '1' },
-    { name: 'Category 2', id: '2' }
+    { name: 'Category 1', id: '1', description: 'Category 1 description', level: '10' },
+    { name: 'Category 2', id: '2', description: 'Category 2 description', level: '20' }
   ]
 
-  renderer.render(<CategoriesList category={categories} handleClickOpen={handleClickOpen} />)
+  renderer.render(<CategoriesList category={categories} handleClickOpen={handleClickOpenDummy} />)
 })
