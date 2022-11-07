41e04cd9ceae8b812e75f3cebd1bfa05608c39c8
Yunus Andréasson <yunus@edenmind.com>
2022-11-07 22:41:25 +0100

    fix(mobile): :sparkles: set correct colors for chips #1462
    Fixes #1462
    
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- mobile/components/selectable-chip.js
+++ mobile/components/selectable-chip.js
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import COLORS from '../constants/colors.js'
+import { paperDarkTheme } from '../constants/paper-theme.js'
 import { Chip } from 'react-native-paper'
 import PropTypes from 'prop-types'
 import { StyleSheet } from 'react-native'
@@ -9,21 +11,25 @@ const styles = StyleSheet.create({
     fontSize: 25
   },
   chipNotSelectedArabic: {
+    backgroundColor: paperDarkTheme.colors.onPrimary,
     direction: 'rtl',
     height: 50,
     margin: 15
   },
   chipNotSelectedEnglish: {
+    backgroundColor: paperDarkTheme.colors.onPrimary,
     direction: 'ltr',
     height: 50,
     margin: 15
   },
   chipSelectedArabic: {
+    backgroundColor: paperDarkTheme.colors.primaryContainer,
     direction: 'rtl',
     height: 50,
     margin: 15
   },
   chipSelectedEnglish: {
+    backgroundColor: paperDarkTheme.colors.primaryContainer,
     direction: 'ltr',
     height: 50,
     margin: 15
@@ -45,8 +51,9 @@ const getStyle = (props) => {
 
 const SelectableChip = (props) => (
   <Chip
-    mode="flat"
+    mode="outlined"
     onPress={props.func}
+    backgroundColor={paperDarkTheme.colors.primary}
     textStyle={props.language === LANGUAGES.arabic ? styles.arabic : styles.english}
     style={getStyle(props)}
   >
