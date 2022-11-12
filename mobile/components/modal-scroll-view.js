e4bd79843e06e887bef40a1838681c4af4e9bd76
Yunus Andréasson <yunus@edenmind.com>
2022-11-12 15:14:01 +0100

    feat(mobile): :lipstick: align button behaviour against material #1540
    Fixes #1540
    
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- mobile/components/modal-scroll-view.js
+++ mobile/components/modal-scroll-view.js
@@ -2,13 +2,14 @@ import { Button, Modal, Portal, Text } from 'react-native-paper'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { ScrollView } from 'react-native'
+import { paperDarkTheme } from '../constants/paper-theme.js'
 
 const ModalScrollView = (props) => {
   const containerStyle = {
     margin: 20,
     padding: 20,
-    backgroundColor: 'rgb(49, 49, 43)',
-    height: '90%',
+    backgroundColor: paperDarkTheme.colors.onPrimary,
+    maxHeight: '90%',
     borderRadius: 10
   }
 
@@ -18,13 +19,19 @@ const ModalScrollView = (props) => {
     paddingRight: 100
   }
 
+  const titleStyle = {
+    alignSelf: 'center'
+  }
+
   return (
     <Portal>
       <Modal visible={props.visible} onDismiss={props.hideModal} contentContainerStyle={containerStyle}>
-        <Text variant="titleMedium">{props.title}</Text>
+        <Text variant="titleLarge" style={titleStyle}>
+          {props.title}
+        </Text>
         <ScrollView>{props.content}</ScrollView>
         <Button onPress={props.hideModal} style={buttonPadding} mode="text">
-          <Text variant="labelMedium">Close</Text>
+          <Text variant="labelSmall">CLOSE</Text>
         </Button>
       </Modal>
     </Portal>
