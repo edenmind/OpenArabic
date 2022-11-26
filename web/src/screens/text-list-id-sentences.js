4c11cf3d5c77aebc3143d9ebbb5fa8ee26f1781a
Yunus Andréasson <yunus@edenmind.com>
2022-11-26 23:57:16 +0100

    fix(web): :bug: only one set of vocabs are now shown #1618
    Fixes #1618
    
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- web/src/screens/text-list-id-sentences.js
+++ web/src/screens/text-list-id-sentences.js
@@ -8,9 +8,11 @@ import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 import DialogContentText from '@mui/material/DialogContentText'
 import DialogTitle from '@mui/material/DialogTitle'
+import { Divider } from '@mui/material'
 
 function TextListIdSentences(properties) {
   const [open, setOpen] = React.useState(false)
+  const [sentencesToShow, setSentencesToShow] = React.useState([])
 
   const handleClickOpen = () => {
     setOpen(true)
@@ -20,41 +22,48 @@ function TextListIdSentences(properties) {
     setOpen(false)
   }
 
-  return properties.sentences.map((sentence, index) => (
+  const sentences = properties.sentences.map((sentence, index) => (
     <Fragment key={index}>
       <Box sx={{ fontSize: 'h4.fontSize', m: 2, paddingTop: '25px' }}>{sentence.arabic}</Box>
       <Box sx={{ m: 2 }}>{sentence.english}</Box>
-      <Button variant="text" onClick={handleClickOpen}>
+      <Button
+        variant="text"
+        onClick={() => {
+          handleClickOpen()
+          setSentencesToShow(sentence.words)
+        }}
+      >
         Vocabulary
       </Button>
-
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle id="alert-dialog-title">{'Vocabulary'}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {sentence.words.map((word, index) => (
-              <Fragment key={index}>
-                <Box sx={{ fontSize: 'h4.fontSize', m: 2 }}>{word.arabic}</Box>
-                <Box sx={{ m: 2 }}>{word.english}</Box>
-              </Fragment>
-            ))}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
-        </DialogActions>
-      </Dialog>
     </Fragment>
   ))
-}
 
-TextListIdSentences.propTypes = {
-  sentences: PropTypes.arrayOf(
-    PropTypes.shape({
-      english: PropTypes.string.isRequired,
-      arabic: PropTypes.string.isRequired
-    })
-  ).isRequired
+  const dialog = (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle id="alert-dialog-title">{'Vocabulary'}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {sentencesToShow.map((word, index) => (
+            <Fragment key={index}>
+              <Box sx={{ fontSize: 'h3.fontSize', m: 2, minWidth: '300px' }}>{word.arabic}</Box>
+              <Box sx={{ m: 2 }}>{word.english}</Box>
+              <Divider />
+            </Fragment>
+          ))}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  )
+
+  return (
+    <Fragment>
+      {sentences}
+      {dialog}
+    </Fragment>
+  )
 }
 
 export default TextListIdSentences
