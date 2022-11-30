1021fb886ca5486b3873eb3e55a9a632a31a9297
Yunus Andréasson <yunus@edenmind.com>
2022-11-30 22:41:04 +0100

    feat(all): :sparkles: add transliteration #1647
    Fixes #1647
    
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- api/services/utils.js
+++ api/services/utils.js
@@ -1,3 +1,5 @@
+/* eslint-disable quote-props */
+
 'use strict'
 
 const { v4: uuidv4 } = require('uuid')
@@ -138,7 +140,98 @@ const slugifyWithAuthor = (title, author) => {
   return `${titleSlug}-${authorSlug}`
 }
 
+//replace every letter in a string based on a map defined in the function
+const transliterateArabicToEnglish = (string) => {
+  let transliteratedArabicToEnglish = ''
+
+  // replace all fatha with the letter a
+  string = string.replace(/َ/g, 'a')
+
+  // replace all kasra with the letter i
+  string = string.replace(/ِ/g, 'i')
+
+  // replace all damma with the letter u
+  string = string.replace(/ُ/g, 'u')
+
+  // replace all alif lam with the letter al
+  string = string.replace(/ﻻ/g, 'al-')
+
+  //remove all shadda
+  string = string.replace(/ّ/g, '')
+
+  //remove all sukon
+  string = string.replace(/ْ/g, '')
+
+  const letterMap = {
+    ٱ: 'a',
+    ء: '',
+    ا: 'ā',
+    ب: 'b',
+    ت: 't',
+    ث: 'ṯ',
+    ج: 'j',
+    ح: 'ḥ',
+    خ: 'ḵ',
+    د: 'd',
+    ذ: 'ḏ',
+    ر: 'r',
+    ز: 'z',
+    س: 's',
+    ش: 'š',
+    ص: 'ṣ',
+    ض: 'ḍ',
+    ط: 'ṭ',
+    ظ: 'ẓ',
+    ع: 'ʿ',
+    غ: 'ġ',
+    ف: 'f',
+    ق: 'q',
+    ك: 'k',
+    ل: 'l',
+    م: 'm',
+    ن: 'n',
+    ه: 'h',
+    و: 'w',
+    ي: 'y',
+    آ: 'ā',
+    ة: 't',
+    ى: 'ā',
+    ـَا: 'ā',
+    ـِي: 'ī',
+    ـُو: 'ū',
+    ـًا: 'ay',
+    ـَو: 'aw',
+    ـً: 'an',
+    ـٍ: 'in',
+    ـٌ: 'un',
+    ' ': ' '
+  }
+
+  for (const letter of string) {
+    const newLetter = letterMap[letter] || letter
+    transliteratedArabicToEnglish += newLetter
+  }
+
+  //replace aā with a
+  transliteratedArabicToEnglish = transliteratedArabicToEnglish.replaceAll('aā', 'ā')
+
+  //replace iī with ī
+  transliteratedArabicToEnglish = transliteratedArabicToEnglish.replaceAll('iī', 'ī')
+
+  //replace uū with ū
+  transliteratedArabicToEnglish = transliteratedArabicToEnglish.replaceAll('uū', 'ū')
+
+  //replace iy with ī
+  transliteratedArabicToEnglish = transliteratedArabicToEnglish.replaceAll('iy', 'ī')
+
+  //replace aٰ with ā
+  transliteratedArabicToEnglish = transliteratedArabicToEnglish.replaceAll('aٰ', 'ā')
+
+  return transliteratedArabicToEnglish
+}
+
 module.exports = {
+  transliterateArabicToEnglish,
   produceVocabularyCollection,
   shuffleArray,
   timeAgo,
