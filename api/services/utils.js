ba044e8b709873e45d011593ca3315b4208e1a99
Yunus Andréasson <yunus@edenmind.com>
2022-12-09 23:41:35 +0100

    feat(api): :sparkles: add call to synthesize in texts controller #1726
    Fixes #1726
    
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- api/services/utils.js
+++ api/services/utils.js
@@ -1,11 +1,40 @@
-//replace every letter in a string based on a map defined in the function
+/* eslint-disable putout/putout */
+/* eslint-disable quote-props */
 
 'use strict'
 
-/* eslint-disable quote-props */
-
-const { v4: uuidv4 } = require('uuid')
 const moment = require('moment')
+const { v4: uuidv4 } = require('uuid')
+const { PutObjectCommand, S3Client } = require('@aws-sdk/client-s3')
+
+//function to copy files from a local directory to a Amazon S3 bucket
+const copyFileToS3 = async (fileContent, fileName) => {
+  const s3Client = new S3Client({
+    endpoint: 'https://ams3.digitaloceanspaces.com',
+    forcePathStyle: false,
+    region: 'ams3',
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+  })
+
+  const parameters = {
+    Bucket: 'openarabic',
+    Body: fileContent,
+    Key: 'audio/' + fileName,
+    ACL: 'public-read'
+  }
+
+  try {
+    const data = await s3Client.send(new PutObjectCommand(parameters))
+    console.log('Successfully uploaded object: ' + parameters.Bucket + '/' + parameters.Key)
+
+    console.log(data)
+  } catch (error) {
+    console.log('Error', error)
+  }
+}
 
 //return time to read based on length of text
 const readingTime = (text) => {
@@ -173,5 +202,6 @@ module.exports = {
   timeAgo,
   readingTime,
   slugifyWithAuthor,
-  mp3Filename
+  mp3Filename,
+  copyFileToS3
 }
