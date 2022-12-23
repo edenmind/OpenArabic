77ed075563905577fcb295dd1d5edb39ff0a5959
Yunus Andréasson <yunus@edenmind.com>
2022-12-23 21:49:12 +0100

    fix(api): :ambulance: add allowList
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- api/plugins/rate-limit.js
+++ api/plugins/rate-limit.js
@@ -5,6 +5,7 @@ const fastifyPlugin = require('fastify-plugin')
 module.exports = fastifyPlugin(async function (fastify) {
   fastify.register(require('@fastify/rate-limit'), {
     max: 300,
-    timeWindow: '1 minute'
+    timeWindow: '1 minute',
+    allowList: ['127.0.0.1']
   })
 })
