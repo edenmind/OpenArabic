b2e7048c595a1d8f6432a229dd44dba9ee742947
Yunus Andréasson <yunus@edenmind.com>
2023-03-30 20:34:13 +0200

    feat(all): :construction_worker: Test Helm Charts with GitHub Action
    Fixes #1359
    
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- package/build.sh
+++ package/build.sh
@@ -4,12 +4,12 @@
 set -e -o pipefail -u
 
 # Define the destination directory and source directory
-DESTINATION="./dist"
-SOURCE="./"
+DESTINATION_DIRECTORY="./dist"
+SOURCE_DIRECTORY="./"
 
 # Lint the Helm chart
-echo "Linting Helm chart in $SOURCE ..."
-if ! helm lint $SOURCE --strict; then
+echo "Linting Helm chart in $SOURCE_DIRECTORY ..."
+if ! helm lint $SOURCE_DIRECTORY --strict; then
     echo "Linting failed!"
     exit 1
 fi
@@ -17,8 +17,8 @@ fi
 echo "Helm chart linted successfully!"
 
 # Package the Helm chart
-echo "Packaging Helm chart into $DESTINATION..."
-if ! helm package $SOURCE --destination $DESTINATION; then
+echo "Packaging Helm chart into $DESTINATION_DIRECTORY..."
+if ! helm package $SOURCE_DIRECTORY= --destination $DESTINATION_DIRECTORY; then
     echo "Packaging failed!"
     exit 1
 fi
