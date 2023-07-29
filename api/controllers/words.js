079cca68725ac74c5a2241989ab84fd0ea8a74b2
Yunus Andréasson <yunus@edenmind.com>
2023-07-29 15:49:29 +0200

    feat(all): :sparkles: Play word in practice
    Fixes #2212
    
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- api/controllers/words.js
+++ api/controllers/words.js
@@ -5,10 +5,15 @@
 /* eslint-disable operator-linebreak */
 
 const COLLECTIONS = require('../constants/collections.js')
-const { capitalizeFirstLetter, shuffleArray, convertToLowerCase, getAllWordsFromTexts } = require('../services/texts')
+const {
+  capitalizeFirstLetter,
+  shuffleArray,
+  convertToLowerCase,
+  getAllWordsFromTexts,
+  removeAnythingBetweenBrackets
+} = require('../services/texts')
 const { ObjectId } = require('mongodb')
 const { timeAgo } = require('../services/utils')
-const { removeAnythingBetweenBrackets } = require('../services/texts')
 
 async function getWordId(request, reply) {
   try {
