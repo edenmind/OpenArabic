8fb500ddcf90e491367b47818e652b5e63de8afd
Yunus Andréasson <yunus@edenmind.com>
2023-08-26 22:33:58 +0200

    feat(mobile): :sparkles: add components
    Fixes #2225
    
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- web/src/services/ai-service.js
+++ web/src/services/ai-service.js
@@ -1,7 +1,7 @@
 import OpenAI from 'openai'
 
 const openai = new OpenAI({
-  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY || '123',
   dangerouslyAllowBrowser: true
 })
 
