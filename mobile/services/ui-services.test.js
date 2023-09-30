20ff46b35cdfe302074fdb6e9f0367ebd03be0eb
Yunus Andréasson <yunus@edenmind.com>
2023-09-30 18:41:50 +0200

    feat(mobile): :lipstick: add guided reading
    Signed-off-by: Yunus Andréasson <yunus@edenmind.com>

--- mobile/services/ui-services.test.js
+++ mobile/services/ui-services.test.js
@@ -1,14 +1,7 @@
-import { generateShare, generateTextError } from './ui-services.js'
+import { generateTextError } from './ui-services.js'
 
 //jest fake timers
 jest.useFakeTimers()
-
-describe('generateShare', () => {
-  it('returns a function', () => {
-    const result = generateShare({})
-    expect(typeof result).toBe('function')
-  })
-})
 
 describe('generateTextError', () => {
   it('returns a function', () => {
